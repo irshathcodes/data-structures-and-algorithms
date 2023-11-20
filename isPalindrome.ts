dd44de9768a3cbcd49515cf3991729664a6f007c
Irshath <irshath700@gmail.com>
2023-11-20 21:43:53 +0530

    palindrome using two pointers approach

--- isPalindrome.ts
+++ isPalindrome.ts
@@ -3,5 +3,33 @@ function isPalindromeUsingRegex(s: string): boolean {
     return cleanStr === cleanStr.split("").reverse().join("");
 }
 
+function isPalindromeUsingTwoPointers(s: string): boolean {
+    let low = 0;
+    let high = s.length - 1;
+
+    function isLowercaseAlphaNumeric(value: string): boolean {
+        return Boolean(
+            (value >= "a" && value <= "z") || (value >= "0" && value <= "9")
+        );
+    }
+
+    while (high >= low) {
+        const firstValue = s[low].toLowerCase();
+        const lastValue = s[high].toLowerCase();
+
+        if (!isLowercaseAlphaNumeric(firstValue)) {
+            low++;
+        } else if (!isLowercaseAlphaNumeric(lastValue)) {
+            high--;
+        } else {
+            if (firstValue !== lastValue) return false;
+            high--;
+            low++;
+        }
+    }
+    return true;
+}
+
 const str = "A man, a plan, a canal: Panama";
 console.log(isPalindromeUsingRegex(str));
+console.log(isPalindromeUsingTwoPointers(str));
